7c034f68e8ae6015efaa66cfe295cdae948d8996
drsdrs <eidenberger_andreas@gmx.at>
2018-04-04 13:00:19 +0200

    otherAdds

--- linkConfigs.sh
+++ linkConfigs.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+## TODO add gpg encryted sensible config files
+
 
 # backup old & link i3 config
 mv ~/.config/i3 ~/.config/i3_old
 ln -sr ./i3 ~/.config/ 
+
+
+# SYSTEM CONFIGS
+# /etc/locale.gen
+sudo mv /etc/locale.gen /etc/locale.gen.old
+sudo cp ./etc/locale.gen /etc/locale.gen
+sudo locale-gen
+
+# bashrc
+mv ~/.bashrc ~/.bashrc.old
+ln -sr ./.bashrc ~/.bashrc
+
+# bash_profile
+mv ~/.bash_profile ~/.bash_profile.old
+ln -sr ./.bash_profile ~/.bash_profile
+
+# zshrc
+mv ~/.zshrc ~/.zshrc.old
+ln -sr ./.zshrc ~/.zshrc
